36ef360841e1acad29cb97bfb268264d4a7dc6db
Marvin Passe <marvinpasse96@gmail.com>
2026-04-01 17:56:26 +0200

    sms massage

--- app/api/twilio-incoming/route.ts
+++ app/api/twilio-incoming/route.ts
@@ -1,3 +1,11 @@
 export async function POST() {
-  return new Response("OK", { status: 200 })
+  return new Response(
+    '<?xml version="1.0" encoding="UTF-8"?><Response></Response>',
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "text/xml"
+      }
+    }
+  )
 }
